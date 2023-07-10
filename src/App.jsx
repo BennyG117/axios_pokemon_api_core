764397ec3be06ca2ade6ccac417f02167ff2ac30
BennyG117 <bengomezp@gmail.com>
2023-07-10 14:28:02 -0700

    Initial Commit - Axios Pokemon Api Core

---
+++ src/App.jsx
@@ -0,0 +1,61 @@
+// import logo from './logo.svg';
+import './App.css';
+import Post from "./components/Post";
+import { styled } from "styled-components";
+import React from "react";
+// import Container from "./components/Post";
+// import FetchButton from "./components/Post";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+function App() {
+  const FetchButton = styled.button`
+    background-color: darkgray;
+    border: none;
+    border-radius: 5px;
+    color: white;
+    display: flex;
+    flex-direction: row;
+    margin: 10px 50px;
+    max-width: 200px;
+    max-height: 75px;
+    justify-content: center;
+
+    /* &:active {
+      {transform: translateY(4px)}
+    }  */
+  `;
+
+  const [pokemonData, setPokemonData] = useState([]);
+
+  
+
+  // const getPokemonList = () => {
+  //   fetch("https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0")
+  //     .then((res) => res.json())
+  //     // .then((data) => console.log(data))
+  //     .then((data) => setPokemonData(data))
+  //     .catch((err) => {
+  //       console.log(err);
+  //     });
+  // };
+
+
+  const getPokemonListAxios = () => {
+    axios.get("https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0")
+    .then (res=> setPokemonData(res.data))
+    .catch(err=> console.log(err))
+  }
+
+  return (
+    <div>
+      <FetchButton onClick={getPokemonListAxios} className="activate">
+        fetch
+      </FetchButton>
+
+      <Post dataProp={pokemonData} />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Post.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { styled } from "styled-components";
+
+
+
+
+const Container = styled.div`
+  background-color: lightgray;
+  border: 1px solid;
+  border-radius: 5px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin:5px 50px;
+  padding: 5px;
+  max-width: 200px;
+`;
+
+
+// function Post (props) {
+const Post = (props) => {
+    //could have deconstructed here, but instead just left as props(See below*)
+    // const {dataProp} = props
+
+
+  return (
+    <>
+
+      <Container>
+        {/* use map to generate UL of all names */}
+        <ul>
+            {props.dataProp.results && props.dataProp.results.map((pokemon) => (
+                <li key={pokemon.name} >{pokemon.name}</li>
+            ))}
+        </ul>
+      </Container>
+    </>
+  );
+};
+//! ^^^ FINISH ABOVE EDIT WITH MAP AND UL to complete***
+
+
+export default Post;
